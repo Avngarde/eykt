98e2c97853868c86823cf1d57d881eac111cd4e2
Avngarde <pivotguy888@gmail.com>
2023-04-04 19:48:00 +0200

    Created EventService and EventTypeService

---
+++ Services/EventService.cs
@@ -0,0 +1,32 @@
+namespace Eykt 
+{
+    public static class EventService
+    {
+        private static EyktContext db = new();
+        public static Event[] GetEvents() 
+        {
+            return db.Events
+                .OrderBy(e => e.Date)
+                .ToArray();
+        }
+
+        public static Event GetEvent(int id) 
+        {
+            return db.Events
+                .Where(b => b.EventId == id)
+                .First();
+        }
+
+        public static void AddEvent(string name, DateOnly date, EventType type, string description) 
+        {
+            Event newEvent = new() 
+            {
+                Name = name,
+                Description = description,
+                Type = type,
+                Date = date
+            };
+            db.Events.Add(newEvent);
+        }
+    }
+}
---
+++ Services/EventTypeService.cs
@@ -0,0 +1,29 @@
+namespace Eykt 
+{
+    public static class EventTypeService
+    {
+        private static EyktContext db = new();
+        public static EventType[] GetTypes() 
+        {
+            return db.EventTypes
+                .OrderBy(e => e.EventTypeId)
+                .ToArray();
+        }
+
+        public static EventType GetEventType(int id) 
+        {
+            return db.EventTypes
+                .Where(b => b.EventTypeId == id)
+                .First();
+        }
+
+        public static void AddEvent(string name) 
+        {
+            EventType newType = new() 
+            {
+                Name = name,
+            };
+            db.EventTypes.Add(newType);
+        }
+    }
+}
